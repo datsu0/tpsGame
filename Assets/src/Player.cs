194d696b29daf7e6e17209297a1366dfecb4b2ec
datsu0 <datsu0@github.com>
2019-12-18 19:08:35 +0900

    make world

--- Assets/src/Player.cs
+++ Assets/src/Player.cs
@@ -7,8 +7,9 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var playerCamera = gameObject.AddComponent<Camera>();
-        
+        //var playerCamera = gameObject.AddComponent<Camera>();
+        var Palyer = GameObject.Find("unitychan");
+        Palyer.transform.Translate(100,0,100);
     }
 
     // Update is called once per frame
